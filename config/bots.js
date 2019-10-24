6ebe7274778aa5eb15d2922aefd2766684c4be45
andrew james faraday <ajfaraday@batman.alces.co.uk>
2019-10-24 13:18:09 +0100

    First try

---
+++ config/bots.js
@@ -0,0 +1,22 @@
+GridGame.data.bots = {
+  'AJFaraday - All Towards': '808c86eb544d9efaf94a30efc91bdfaf',
+  'AJFaraday - Faraday Cage': 'd9eb9a70ad0dc0fb1885be0fce032adc',
+  'AJFaraday - Long Random': 'b9a0fe99060d9be1fc617bb7262f57be',
+  'AJFaraday - Rotate': '6634a7f381bb679ee7c2d90d93588d66',
+  'AJFaraday - Short Random': 'b16df33acb0f0c7830d2ca2d656f80be',
+  'Alion - Doombeam': '496367ff51c27dc62ebdc473cd14f333',
+  'Alion - Evolver: First Encounter': 'c0382c11979adc1f47773714ffc8376f',
+  'Alion - Evolver: Overnight': '7512fcfcef33bff323d5cdaf1bb833ff',
+  'Alion - Rushdown': '5a989c80bf1bbef32c848f248aa55dcb',
+  'Alion - Slideout': '056b16e0dc85ee2f13cc6061cf8162ca',
+  'Alion - Weirdflow': '8f82f8a5fd15fc29e60b9cce4343309c',
+  'Alion - Zerg Rush': '1f8b3963656f9e15577f886e0125a343',
+  'AllirionX - 3 Base rush': '3f6d1a1461564ace3d7e22cedba08c20',
+  'BradC - Double Barrel': '7d0a88c1533fc89b2ee26484bbcafc59',
+  'BradC - Sweeping Endgame': '3f69bdf2be37886ff396d0c485007e80',
+  'BradC - Quad Rush': 'a0a4a9f3c8b8863392f109fdfd52af00',
+  'Brilliand - Backstabber': '2d632fb11ea1a7b1b73788d396a6a45d',
+  'Brilliand - Galeforce': '294c357b051e6e165d18657748d02cfe',
+  'Brilliand - Line Drive': '54e4de5b03423cc19a1ec0e22f32a981'
+};
+
